6a9d134dab9688a56cda83af79d2080fbcd9fcbd
zengdz <zeng.zhen@foxmail.com>
2017-05-16 21:19:44 +0800

    CH3-批量处理数据
    第三章的两个讲解例程

---
+++ ch3_batch_of_data/3.1_batch_of_data.cpp
@@ -0,0 +1,59 @@
+﻿#include <iomanip> // setprecision
+#include <ios> // streamsize
+#include <iostream> // cin,cout,endl
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::string;
+using std::streamsize;
+
+int main()
+{
+	// ask for and read the student's name 
+	cout << "Please enter your first name: ";
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	
+	// ask for and read the midterm and final grades
+	cout << "Please enter your midterm and final exam grades: ";
+	double midterm, final;
+	cin >> midterm >> final;
+	
+	// ask for homework grades
+	// 空格隔开的字符串可以自动拼接一起
+	cout << "Enter all your homework grades, "
+			"followed by end-of-file: ";
+			
+	// the number and sum of grades read so far
+	int count = 0;
+	double sum = 0;
+	
+	// a variable into which to read
+	double x;
+	
+	// invariant:
+	// 目前为止，已经读入count个homework成绩
+	// 并且sum等于头count个成绩的总和
+	// hit Ctrl-Z to end-of-file
+	// cin >> x 不光是判断条件：结果是cin，可以转换为bool类型用于判断
+	// 还有副作用：读入一个值到x，需考虑其对不变式的影响
+	while (cin >> x){
+		++count;
+		sum += x;
+	}
+	
+	// 获取cout默认的精度，在输出指定精度的内容之后恢复为默认
+	streamsize prec = cout.precision();
+	
+	// write the result
+	// setprecision(3) 表示三位有效数，改变后面输出流的格式
+	cout << "Your final grade is " << setprecision(3)
+		 << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+		 << setprecision(prec) << endl;
+		 
+	return 0;
+}
---
+++ ch3_batch_of_data/3.2_batch_of_data.cpp
@@ -0,0 +1,75 @@
+﻿/* 如果只是计算homework平均值，那么没有必要保存每个输入的homework的成绩
+   但是，如果是取中值而非平均值，那么必须把输入的homework保存下来，使用vector吧*/
+#include <iomanip> // setprecision
+#include <ios> // streamsize
+#include <iostream> // cin,cout,endl
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::string;
+using std::streamsize;
+using std::vector;
+using std::sort;
+
+int main()
+{
+	// ask for and read the student's name 
+	cout << "Please enter your first name: ";
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	
+	// ask for and read the midterm and final grades
+	cout << "Please enter your midterm and final exam grades: ";
+	double midterm, final;
+	cin >> midterm >> final;
+	
+	// ask for homework grades
+	// 空格隔开的字符串可以自动拼接一起
+	cout << "Enter all your homework grades, "
+			"followed by end-of-file: ";
+	
+	// a variable into which to read
+	double x;
+	vector <double> homework;
+	
+	// invariant:
+	// 目前为止，已经读入count个homework成绩
+	// hit Ctrl-Z to end-of-file
+	// vector.push_back(x)结果是把x添加到向量尾部
+	// 副作用是把向量的长度加一
+	while(cin >> x){
+		homework.push_back(x);
+	}
+
+	// 检查homework输入是否为空
+	typedef vector<double>::size_type vec_sz;
+	vec_sz size = homework.size();
+	if (size == 0) {
+		cout << endl << "You must enter your grades. "
+			"Please try again." << endl;
+		return 1;
+	}
+
+	// 对homework成绩进行排序
+	sort (homework.begin(), homework.end());
+
+	// 计算homework成绩的中值
+	vec_sz mid = size / 2;
+	double median;
+	median = size % 2 == 0 ? (homework[mid] + homework[mid - 1]) / 2 : homework[mid];
+
+	// 计算并输出总成绩
+	streamsize prec = cout.precision();
+	// setprecision(3) 表示三位有效数
+	cout << "Your final grade is " << setprecision(3)
+		 << 0.2 * midterm + 0.4 * final + 0.4 * median
+		 << setprecision(prec) << endl;
+		 
+	return 0;
+}
