96562b67c4b3c72e13a03ce97ff9ac1f5aa5d3e3
zengdz <zeng.zhen@foxmail.com>
2017-05-19 10:38:13 +0800

    练习setw对齐

---
+++ ch4_organize_program/Ex4.2.cpp
@@ -0,0 +1,22 @@
+﻿/*  计算1-100整数值对应的平方
+	输出分两列并且对齐排列 */
+#include <iostream> // cin,cout,endl
+#include <iomanip> //setw
+#include <ios> //streamsize
+#include <vector>
+
+using namespace std;
+
+int main()
+{ 
+	cout << "The numbers 1-100 squares are: " << endl;
+	streamsize wh = cout.width();
+	for (int i = 1; i < 101; ++i) {
+		// 1-100最大三位数，其平方最大五位数
+		cout << setw(3) << i << " : " 
+			 << setw(5) << i * i << setw(wh) << endl;
+	}
+
+	return 0;
+}
+
---
+++ ch4_organize_program/Ex4.3.cpp
@@ -0,0 +1,41 @@
+﻿/*  计算指定范围的整数值对应的平方
+	输出分两列并且对齐排列，自动对齐 */
+#include <iostream> // cin,cout,endl
+#include <iomanip> //setw
+#include <ios> //streamsize
+
+using namespace std;
+
+// 获取整数值的宽度，返回该宽度值
+char getWidth(int num)
+{
+	char width = 0;
+	while (num > 0) {
+		++width;
+		num /= 10;
+	}
+	return width;
+}
+
+int main()
+{ 
+	cout << "Please enter the begin and end number: " << endl;
+	int beg, end;
+	cin >> beg >> end;
+
+	// 如果起始大于终止，则输出提示并退出
+	if (beg > end) {
+		cout << "Begin must less equal than end!" << endl;
+		return 1;
+	}
+
+	cout << "The numbers and squares are: " << endl;
+	streamsize wh = cout.width();
+	for (int i = beg; i < end + 1; ++i) {
+		cout << setw(getWidth(end)) << i << " : " 
+			 << setw(getWidth(end * end)) << i * i << setw(wh) << endl;
+	}
+
+	return 0;
+}
+
